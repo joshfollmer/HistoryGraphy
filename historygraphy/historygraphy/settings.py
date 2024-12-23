d84d40ab5c0ba7d2ea4c8a37793290da54767ab2
joshfollmer <josh.follmer@icloud.com>
2024-12-22 22:13:53 -0800

    added basic homepage

--- historygraphy/historygraphy/settings.py
+++ historygraphy/historygraphy/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'historygraphy',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'historygraphy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
